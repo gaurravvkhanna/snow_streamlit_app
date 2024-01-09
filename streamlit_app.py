06afa71b86913bc7e117420638e961092b171145
gaurravvkhanna <82760363+gaurravvkhanna@users.noreply.github.com>
2024-01-09 17:55:29 -0500

    Update streamlit_app.py
    start using pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 st.title("Welcome to my parents diner")
 
 st.header("Breakfast Favorites")
@@ -8,3 +9,5 @@ st.text("🐔Hard-Boiled Free Range Egg")
 st.text("🥑🍞Avocado Toast")
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
