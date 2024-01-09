e3745fe4d8d71a943e8b81da6a6f685cb48e1aa6
gaurravvkhanna <82760363+gaurravvkhanna@users.noreply.github.com>
2024-01-09 14:26:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 st.title("Welcome to my parents diner")
 
+st.header("Breakfast menu")
+st.text("Omega 3 & Blueberry oatmeal")
+st.text("Kale, Spinach & Rocket Smoothie")
+st.text("Hard-Boiled Free Range Egg")
