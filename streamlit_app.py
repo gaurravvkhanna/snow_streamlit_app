40d47ae779c8890c9adf955b742987ad56bf84fb
gaurravvkhanna <82760363+gaurravvkhanna@users.noreply.github.com>
2024-01-09 17:52:32 -0500

    Update streamlit_app.py
    Added icons to menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 st.title("Welcome to my parents diner")
 
-st.header("Breakfast menu")
-st.text("Omega 3 & Blueberry oatmeal")
-st.text("Kale, Spinach & Rocket Smoothie")
-st.text("Hard-Boiled Free Range Egg")
+st.header("Breakfast Favorites")
+st.text("🥣Omega 3 & Blueberry oatmeal")
+st.text("🥗Kale, Spinach & Rocket Smoothie")
+st.text("🐔Hard-Boiled Free Range Egg")
+st.text("🥑🍞Avocado Toast")
+
