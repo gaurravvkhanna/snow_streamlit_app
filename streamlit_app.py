9f23a7006b0949ea030f43df537ca7ddf6fcba3a
gaurravvkhanna <82760363+gaurravvkhanna@users.noreply.github.com>
2024-01-13 14:59:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,5 +40,5 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 st.text("Hello from Snowflake:")
-st.text(my_data_row)
+st.dataframe(my_data_row)
 
