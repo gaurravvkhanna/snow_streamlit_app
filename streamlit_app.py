ea634ca0ff87674b23d70f1843fd3a769e6de0cc
gaurravvkhanna <82760363+gaurravvkhanna@users.noreply.github.com>
2024-01-13 08:20:56 -0500

    Update streamlit_app.py
    Add request to fruityvice API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import requests
+
 st.title("Welcome to my parents diner")
 
 st.header("Breakfast Favorites")
@@ -18,3 +20,7 @@ fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 st.dataframe(fruits_to_show)
+
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
